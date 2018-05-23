34e562cca465605fd7df32c377b14a23ae2db269
qianxingW <983877745@qq.com>
2018-05-23 19:00:47 +0800

    .

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Router from 'vue-router'
 import App from './App'
 import router from './router'
+Vue.use(Router)
 
-Vue.config.productionTip = false
+const routes = new Router({
+  router
+})
+
+Vue.config.productionTip = false // 设置为 false 以阻止 vue 在启动时生成生产提示。
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
+  routes,
+  runder: h => h(App)
+}).$mount('#app')
---
+++ src/router/router.js
@@ -0,0 +1,10 @@
+
+import HelloWorld from '@/components/HelloWorld'
+
+export default [
+  {
+    path: '/',
+    name: 'HelloWorld',
+    component: HelloWorld
+  }
+]
