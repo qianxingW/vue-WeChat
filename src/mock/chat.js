63ce27f864005f8747e3e5cd3bfde3aa910125fe
qianxingw <983877745@qq.com>
2018-05-26 22:21:16 +0800

    。

--- src/mock/chat.js
+++ src/mock/chat.js
@@ -5,8 +5,23 @@ export default [
     nickname: '马小云', // 昵称
     sex: '0', // 性别 0 男 1 女
     remarkname: '马小云', // 备注
-    wx_id: 'wx_hb', // 微信号
+    wx_id: 'wx_my', // 微信号
     qq: '000000',
+    area: ['中国', '北京', '北京'], // 地区
+    type: 'friend',
+    Msg: [{
+      text: '11',
+      endTimeStr: 1472632586443
+    }]
+  },
+  {
+    id: '1',
+    name: '马化腾',
+    nickname: '马小腾', // 昵称
+    sex: '0', // 性别 0 男 1 女
+    remarkname: '马小腾', // 备注
+    wx_id: 'wx_mht', // 微信号
+    qq: '000001',
     area: ['中国', '北京', '北京'], // 地区
     type: 'friend',
     Msg: [{
