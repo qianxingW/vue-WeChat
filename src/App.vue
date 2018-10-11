cf0ba0f800fdb357b8ef73d03e3c6bf81a26d052
weiqianxing <weiqianxing@togeek.cn>
2018-10-11 11:29:41 +0800

    测试merge

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+   <!-- 同步更改测试 -->
     <div id="wx-app">
       <!-- 顶部显示 第二次测试撤回 版本测试 分支测试 two分支测试-->
       <header id="wx-header" :class="{'header-hide':seachStatus}">
